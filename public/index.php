b2eb0c3c72cdd38a0fac8610ddfea6f4519f8ea9
Bjarke Hou Kammersgaard <bjarkehou@Bjarkes-MacBook-Pro.local>
2018-08-05 06:49:06 +0200

    asd

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'hobofo';
+$config['db']['pass']   = 'test';
+$config['db']['dbname'] = 'hobofo';
+
+$app = new \Slim\App(['settings' => $config]);
+
+$container = $app->getContainer();
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    $this->logger->addInfo('Response sent..');
+    return $response;
+});
+$app->run();
